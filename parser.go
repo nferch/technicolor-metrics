248aae04079ec7b3f7737ce94de387efa863d4ff
Nathan A. Ferch <nf@marginal.net>
2017-09-27 16:14:36 +0200

    add upstream parsing

--- parser.go
+++ parser.go
@@ -27,6 +27,21 @@ type DownstreamResult struct {
 	Modulation string
 }
 
+// UpstreamResultList is the stats for downstream.
+type UpstreamResultList struct {
+	UpstreamChannels []UpstreamResult
+}
+
+// UpstreamResult is a single channel's stats
+type UpstreamResult struct {
+	Index      uint8
+	LockStatus string
+	Frequency  int16
+	SymbolRate int16
+	Power      float32
+	Modulation string
+}
+
 func (s *Stats) Downstream() (*DownstreamResultList, error) {
 	// #content > div:nth-child(6)
 	// s.body.Find("//*[@id=\"module\"]/table/thead/tr/th[1]").Each(func(i int, s *goquery.Selection) {
@@ -36,6 +51,17 @@ func (s *Stats) Downstream() (*DownstreamResultList, error) {
 		return nil, err
 	}
 	return parseDownstream(tab)
+}
+
+// Upstream parses upstream stats from HTML
+func (s *Stats) Upstream() (*UpstreamResultList, error) {
+	// #content > div:nth-child(6)
+	//*[@id="content"]/div[5]/table/thead/tr/th[1]
+	tab, err := findStatsTable(s.body, "Upstream")
+	if err != nil {
+		return nil, err
+	}
+	return parseUpstream(tab)
 }
 
 func findStatsTable(sel *goquery.Document, direction string) (*goquery.Selection, error) {
@@ -87,4 +113,39 @@ func parseDownstream(sel *goquery.Selection) (*DownstreamResultList, error) {
 		})
 	})
 	return &drl, nil
+}
+
+func parseUpstream(sel *goquery.Selection) (*UpstreamResultList, error) {
+	url := UpstreamResultList{}
+	rowTitle := ""
+	sel.Find("tbody > tr").Each(func(i int, tr *goquery.Selection) {
+		// fmt.Printf("tr %#v\n", tr)
+		tr.Find("th").Each(func(j int, th *goquery.Selection) {
+			// fmt.Printf("th %#v\n", th.Text())
+			rowTitle = th.Text()
+		})
+		tr.Find("td").Each(func(j int, td *goquery.Selection) {
+			// fmt.Printf("td %d %#v\n", j, td.Text())
+			if len(url.UpstreamChannels) < j+1 {
+				url.UpstreamChannels = append(url.UpstreamChannels, UpstreamResult{})
+			}
+			tdint, _ := strconv.Atoi(strings.Split(strings.TrimSpace(td.Text()), " ")[0])
+			tdfloat, _ := strconv.ParseFloat(strings.Split(strings.TrimSpace(td.Text()), " ")[0], 32)
+			switch rowTitle {
+			case "Index":
+				url.UpstreamChannels[j].Index = uint8(tdint)
+			case "Lock Status":
+				url.UpstreamChannels[j].LockStatus = td.Text()
+			case "Frequency":
+				url.UpstreamChannels[j].Frequency = int16(tdint)
+			case "Symbol Rate":
+				url.UpstreamChannels[j].SymbolRate = int16(tdint)
+			case "Power":
+				url.UpstreamChannels[j].Power = float32(tdfloat)
+			case "Modulation":
+				url.UpstreamChannels[j].Modulation = td.Text()
+			}
+		})
+	})
+	return &url, nil
 }
--- parser_test.go
+++ parser_test.go
@@ -29,3 +29,37 @@ func TestDownstream(t *testing.T) {
 		}
 	}
 }
+
+func TestUpstream(t *testing.T) {
+	fixture, err := os.Open(fixtureFilename)
+	if err != nil {
+		t.Fatalf("Can't open fixture file: %v", err)
+	}
+
+	sel, derr := goquery.NewDocumentFromReader(fixture)
+	if derr != nil {
+		t.Fatalf("Can't parse fixture file: %v", derr)
+	}
+
+	s := &Stats{body: sel}
+	url, userr := s.Upstream()
+	if userr != nil {
+		t.Errorf("Error Upstream(): %v", userr)
+	}
+	for i := 0; i < Max(len(url.UpstreamChannels), len(fixtureUsr.UpstreamChannels)); i++ {
+		if len(url.UpstreamChannels)-1 < i {
+			t.Errorf("Upstream() missing results for idx %d, should be %#v\n", i, fixtureUsr.UpstreamChannels[i])
+		} else if len(fixtureUsr.UpstreamChannels)-1 < i {
+			t.Errorf("Upstream() missing results for idx %d, should be %#v\n", i, url.UpstreamChannels[i])
+		} else if url.UpstreamChannels[i] != fixtureUsr.UpstreamChannels[i] {
+			t.Errorf("Upstream() results differ for %d: %#v vs %#v", i, url.UpstreamChannels[i], fixtureUsr.UpstreamChannels[i])
+		}
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
